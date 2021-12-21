06ee952b8faa2862d7630391b627a944b561de6d
djungeldan <79529185+djungeldan@users.noreply.github.com>
2021-12-21 12:08:13 +0000

    Added input system and marshalsec builder

--- Log4Me.py
+++ Log4Me.py
@@ -1,8 +1,7 @@
 ##############
 #LOG4ME
-#EDUCATIONAL PURPOSES ONLY
-#DONT DO ILLEGAL SHIT
-#Dan, comment this shit when you get a chance pls
+#EDUCATIONAL AND TESTING PURPOSES ONLY
+#DONT DO ILLEGAL SHIT PLEASE <3
 ##############
 
 import os
@@ -21,31 +20,61 @@ dirlist = os.listdir("./")
 for i in dirlist:
     if i != "marshalsec-master":
         downloaded = False
-        break
+        continue
     else:
         downloaded = True
         print("Marshalsec downloaded!")
-        continue
+        break
 
 if downloaded == False:
     print("Marshalsec was not downloaded successfully, or the permissions in this directory are not configured "
           "correctly. Please download it manually from http://https://github.com/mbechler/marshalsec , or check your "
           "permissions.")
     exit(69)
-#Script to get maven if not installed already, and then
+
+#function to get maven if not installed already, and then build marshalsec
+#this fucking sucks btw, its just running system commands but i cant be fucked trying to learn how to build
+#a shitting java package in python
+#It also lets the program know if it built properly so ig thats a plus
+#TODO Check for marshalsec if its already built
+
 def buildMSec():
+    global marshalcheck
+
+    #Updates package lists and gets maven from apt if not already there
     print("Checking for maven... ")
     os.system("sudo apt-get update")
     stream = os.popen("sudo apt-get install maven")
     print(stream.read())
+
+    #moves into marshalsec folder to build
     os.system("cd marshalsec-master")
+
+    #disclaimer please dont read this this fucking sucks but its the only way my monkey brain
+    #can think of doing it
+
+    marshalexist = os.listdir("./")
+    marshalcheck = False
+    for i in marshalexist:
+        if i != "target":
+            continue
+        else:
+            os.system("cd target")
+            os.listdir("./")
+
+
     print("Building...")
+
+    #build marshalsec using maven (hopefully. probably wont work and you'll have to fucking fix it ahduifphapisfdhpai)
     buildresult = os.system("mvn clean package -DskipTests")
     if buildresult != 0:
-        print("Something went wrong when building marshalsec. Try again or manually build using mvn clean package "
-              "-DskipTests when in the marshalsec-master directory.")
+        return("n")
     else:
         print("Marshalsec built.")
+        marshalcheck = True
+        return("y")
+
+#
 def getInfo():
     global ip
     global port
@@ -60,13 +89,14 @@ def getInfo():
                 port = 80
         else:
             port = port
+def marshalsecInit():
+    print("nothing here yet, this is where marshalsec should be run")
+
 
 
 #TODO COMMENT YA DAMN CODE
 
 #TODO Finish input system (make it a bit better maybe)
-
-#TODO Check for marshalsec if its already built
 
 #TODO Add compiler for the java file, would also be nice to have it auto generate the exploit for you
 
